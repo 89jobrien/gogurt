2a13cd687657d712d5161744843afb5e22129d2b
jprobrien <98370624+89jobrien@users.noreply.github.com>
2025-08-19 22:15:20 -0400

    Update vectorstore.go

--- vectorstores/vectorstore.go
+++ vectorstores/vectorstore.go
@@ -9,4 +9,3 @@ type VectorStore interface {
 	AddDocuments(ctx context.Context, docs []types.Document) error
 	SimilaritySearch(ctx context.Context, query string, k int) ([]types.Document, error)
 }
-
